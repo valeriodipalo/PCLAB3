b466d1ddeda756c079cb8a0623ec338b5bcfc4d0
valeriodipalo <82063143+valeriodipalo@users.noreply.github.com>
2022-09-28 12:57:05 +0200

    Task 2 + some minor adjustment in code

--- functions.py
+++ functions.py
@@ -1,3 +1,9 @@
+import re
+import spacy
+nlp = spacy.load('en_core_web_sm')
+import nltk
+from nltk.corpus import stopwords
+
 def word_count_col(df, text, new_col_name):
     """This function will define the number of words in each row of the considered dataframe
     df: dataframe to use
@@ -10,6 +16,32 @@ def word_count_col(df, text, new_col_name):
     df[new_col_name] = words
     return df
 
-def hello_what(word):
-    print('hello ' + str(word))
+def preprocess(input_text):
+    """This function will allow us to perform all the preprocessing steps to reduce noise in text data. This is fundamental to perform text analysis.
+    The function will return a list of cleaned sentences"""
+
+    # Normalize to lowercase
+    txt = input_text.lower()
+
+    # Remove URLs
+    txt = re.sub('https?://\S+|www\.\S+', '', txt)
+
+    # Remove numbers
+    txt = re.sub(r"\d", "", txt)
+
+    # Remove punctuation
+    txt = re.sub('\n', '', txt)
+    txt = re.sub(r"[^A-Za-z]+", " ", txt)
+
+    # Remove stopwords
+    stop_words = [w.lower() for w in stopwords.words('english')]
+    stop_words = set(stop_words).difference(set(['against','above','below', 'over', 'under']))
+    txt = ' '.join([w for w in txt.split() if w not in stop_words and w !='user'])
+
+    # Lemmatization
+    lemmas = [token.lemma_ for token in nlp(txt)]
+    txt = " ".join(lemmas)
+
+    return txt
+
 
