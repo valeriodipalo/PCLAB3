c266bc68bb85c3a8ef198d7d1c48b576622e714d
valeriodipalo <82063143+valeriodipalo@users.noreply.github.com>
2022-10-03 13:49:58 +0200

    Update requirements

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press ⇧⏎ to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import tensorflow
 
 
 def print_hi(name):
